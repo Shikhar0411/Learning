5e0656069a4e65e08a4ae1799c9354c461f3dafe
shivohra <shivohra@paypal.com>
2023-11-24 00:47:29 +0530

    basic JS learning

---
+++ javascript/variables.js
@@ -0,0 +1,8 @@
+var myName = "Shikhar";
+// var variables have scope throughout the project
+
+let myName2 = "Shikhar";
+// let variables have limited scope
+
+const pi = 3.14;
+// const variables are never changed throughout the program
